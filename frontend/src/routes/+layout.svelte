9b0a1cac3c1a17f78b1b91940ec7fa7d1351c9b8
b-sharman <byron.n.sharman@gmail.com>
2024-04-13 15:02:54 -0500

    make the header a link to the root route

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -3,6 +3,6 @@
 </script>
 
 <header class="fixed w-full flex justify-center place-content-center py-5">
-  <p class="font-extrabold text-xl">Our App Name</p>
+  <a href="/" alt="return to home page" class="font-extrabold text-xl">Our App Name</a>
 </header>
 <slot></slot>
