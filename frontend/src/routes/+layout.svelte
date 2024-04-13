2e3a92188487526272a65a1605aba1d0403719d6
b-sharman <byron.n.sharman@gmail.com>
2024-04-13 11:04:01 -0500

    fix awkward scrolling caused by header

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -2,7 +2,7 @@
   import "../app.css";
 </script>
 
-<header class="flex justify-center place-content-center py-5">
+<header class="fixed w-full flex justify-center place-content-center py-5">
   <p class="font-extrabold text-xl">Our App Name</p>
 </header>
 <slot></slot>
