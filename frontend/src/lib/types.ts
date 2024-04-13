edc842e010852d627f48ee2fde3fbe62e40b0db7
Tyler Wright <tyler.wright5146@gmail.com>
2024-04-13 15:41:24 -0500

    pairs type

--- frontend/src/lib/types.ts
+++ frontend/src/lib/types.ts
@@ -7,3 +7,18 @@ export type SelectItem = {
   href: string;
   label: string;
 }
+
+export type Patient = {
+  id: string;
+  name: string;
+  id_string: string;
+}
+
+export type Pair = {
+  id: string;
+  patient_id: string;
+  object_one_type: string;
+  object_one_value: string;
+  object_two_type: string;
+  object_two_value: string;
+}
---
+++ frontend/src/routes/manage/[slug]/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    import { onMount } from 'svelte';
+
+    import type { Patient } from '$lib/types';
+  
+    const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+    export let data: PageData;
+
+    let patient: Patient;
+
+    onMount(async () => {
+        console.log(data);
+        const res = await fetch(`${backendUrl}/patients/${data.slug}`);
+        patient = await res.json();
+    });
+    
+  </script>
+
+    <h1>{patient.name}'s Profile</h1>
+
+
+
+  
