d3039ccc0601ccf45e97ef4777b0d4bb2cf817ff
Tyler Wright <tyler.wright5146@gmail.com>
2024-04-13 13:41:43 -0500

    add patient form

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -3,6 +3,50 @@
   import { Button, Modal, Label, Input } from 'flowbite-svelte';
   import { PlusOutline } from 'flowbite-svelte-icons';
   let formModal = false;
+
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+  async function handleSubmit(event: any) {
+    event.preventDefault();
+
+    console.log(event.target);
+
+    const formData = new FormData(event.target);
+
+    console.log(formData); 
+
+    const patientName: string = formData.get('patientName')?.toString() || '';
+
+    if (!patientName) {
+      alert('Please enter a patient name');
+      return;
+    }
+
+    const id_string = btoa(patientName)
+      .replace(/=/g, '')
+      .replace(/\+/g, '_')
+      .replace(/\//g, '_')
+      .toLowerCase();
+    
+    try {
+      const response = await fetch(`${backendUrl}/patient/`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ id_string: id_string, name: patientName }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      formModal = false;
+      location.reload();
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
 </script>
 
 <Selection heading={"Choose a Patient"} options={["Bob", "Janet", "Ivan", "Dorothy"]} />
@@ -14,11 +58,11 @@
 </div>
 
 <Modal bind:open={formModal} size="md" autoclose={false} class="w-full">
-  <form class="flex flex-col space-y-6" action="#">
+  <form class="flex flex-col space-y-6" on:submit={handleSubmit}>
     <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Add a Patient</h3>
     <Label class="space-y-2">
       <span>Patient Name</span>
-      <Input type="text" placeholder="Enter patient name" required />
+      <Input name="patientName" type="text" placeholder="Enter patient name" required />
     </Label>
     <br>
     <Button type="submit" class="w-full1" color="blue">
