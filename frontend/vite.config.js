61c88d13168f9f5fbb4b7431da076fda96785be6
Tyler Wright <tyler.wright5146@gmail.com>
2024-04-13 01:01:03 -0500

    make docker fontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,5 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	preview: {
+		port: 8080,
+		strictPort: true,
+		host: "0.0.0.0",
+	}
 });
