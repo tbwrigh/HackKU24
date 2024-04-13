23dd6cd98ac6dac0a73c0de1e50c28c6a190fa04
b-sharman <byron.n.sharman@gmail.com>
2024-04-13 09:54:12 -0500

    draft of selection panel

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,20 +1,15 @@
 <script lang="ts">
-  import { Alert, Label, Input } from 'flowbite-svelte';
-  import { ElementType } from '$lib/types.ts';
-  import Element from '$lib/element.svelte';
-
-  let in0val = "";
-  let in1val = "";
+  import { Button, ButtonGroup, Heading } from 'flowbite-svelte';
 </script>
 
-<div>
-  <Label for="default-input" class="block mb-2">Item 1</Label>
-  <Input id="default-input" name="in0" placeholder="first match" bind:value={in0val} />
-  <Label for="default-input" class="block mb-2">Item 2</Label>
-  <Input id="default-input" name="in1" placeholder="second match" bind:value={in1val} />
-  {#if in0val!=="" && in0val==in1val}
-    <Alert>They match!</Alert>
-  {/if}
+<div class="w-screen h-screen flex justify-center place-content-center">
+  <div class="flex flex-col place-content-center">
+    <div class="w-[65ch] flex flex-col gap-4">
+      <Heading tag="h1" class="text-4xl text-center text-black">Choose a Patient</Heading>
+      <Button href="/" size="lg" color="light" class="mx-4 hover:ml-8 hover:mr-0 hover:bg-white hover:shadow hover:font-bold transition-all" pill>Patient 1</Button>
+      <Button href="/" size="lg" color="light" class="mx-4 hover:ml-8 hover:mr-0 hover:bg-white hover:shadow hover:font-bold transition-all" pill>Patient 2</Button>
+      <Button href="/" size="lg" color="light" class="mx-4 hover:ml-8 hover:mr-0 hover:bg-white hover:shadow hover:font-bold transition-all" pill>Patient 3</Button>
+      <Button href="/" size="lg" color="light" class="mx-4 hover:ml-8 hover:mr-0 hover:bg-white hover:shadow hover:font-bold transition-all" pill>Patient 4</Button>
+    </div>
+  </div>
 </div>
-
-<Element value={"hello!"} type={ElementType.Text}/>
