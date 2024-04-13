d63744ca11e67d7a9fc0318972ef0db7b269d62a
b-sharman <byron.n.sharman@gmail.com>
2024-04-13 08:14:09 -0500

    add flashcard prototype

---
+++ frontend/src/lib/flashcard.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { Card } from 'flowbite-svelte';
+
+  let flipped: boolean = false;
+
+</script>
+
+<div class="flashcard-container p-4">
+  <Card on:click={() => flipped = !flipped} style={`perspective: 700px; transition: transform 0.3s; ${flipped ? "transform: rotateY(0.5turn)" : ""}`}>
+    {#if flipped}
+      <div style="transform: rotateY(0.5turn)">
+        <slot name="back" />
+      </div>
+    {:else}
+      <div>
+        <slot name="front" />
+      </div>
+    {/if}
+  </Card>
+</div>
+
+<style>
+  .flashcard-container {
+    perspective: 1000px;
+    transform-style: preserve-3d;
+  }
+</style>
