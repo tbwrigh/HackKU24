06a8120500e8bdaa73377451c5e0de5de8195710
b-sharman <byron.n.sharman@gmail.com>
2024-04-13 10:22:49 -0500

    add a quick header

--- frontend/src/lib/selection.svelte
+++ frontend/src/lib/selection.svelte
@@ -5,13 +5,11 @@
   export let options: string[];
 </script>
 
-<div class="w-screen h-screen flex justify-center place-content-center">
-  <div class="flex flex-col place-content-center">
-    <div class="w-[65ch] flex flex-col gap-4">
-      <Heading tag="h1" class="text-4xl text-center text-black">{heading}</Heading>
-      {#each options as option}
-        <Button href="/" size="lg" color="light" class="mx-4 hover:ml-8 hover:mr-0 hover:bg-white hover:shadow hover:font-bold transition-all" pill>{option}</Button>
-      {/each}
-    </div>
+<div class="flex w-dvw h-dvh justify-center items-center place-content-center">
+  <div class="w-[65ch] flex flex-col gap-4">
+    <Heading tag="h1" class="text-4xl text-center text-black">{heading}</Heading>
+    {#each options as option}
+      <Button href="/" size="lg" color="light" class="mx-4 hover:ml-8 hover:mr-0 hover:bg-white hover:shadow hover:font-bold transition-all" pill>{option}</Button>
+    {/each}
   </div>
 </div>
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,1 +1,8 @@
-<script>import "../app.css";</script><slot></slot>
+<script>
+  import "../app.css";
+</script>
+
+<header class="flex justify-center place-content-center py-5">
+  <p class="font-extrabold text-xl">Our App Name</p>
+</header>
+<slot></slot>
