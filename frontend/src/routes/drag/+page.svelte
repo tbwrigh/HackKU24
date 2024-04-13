4d0207d98474f3ed37f0cf30723bc1d58a373bde
daghassi <79544966+daghassi@users.noreply.github.com>
2024-04-12 23:57:17 -0500

    drag works

---
+++ frontend/src/routes/drag/+page.svelte
@@ -0,0 +1,43 @@
+<script>
+	
+	function dragMe(node) {
+		 let moving = false;
+		 let left = 300;
+		 let top = 100;
+
+		 node.style.position = 'absolute';
+		 node.style.top = `${top}px`;
+		 node.style.left = `${left}px`;
+		 node.style.cursor = 'move';
+		 node.style.userSelect = 'none';
+
+		 node.addEventListener('mousedown', () => {
+			 moving = true;
+		 });
+		 
+		window.addEventListener('mousemove', (e) => {
+			  if (moving) {
+					 left += e.movementX;
+					 top += e.movementY;
+					 node.style.top = `${top}px`;
+					 node.style.left = `${left}px`;
+				}
+		 });
+		
+		 window.addEventListener('mouseup', () => {
+			 moving = false;
+		 });
+	
+	}
+</script>
+
+<style>
+	h1 {
+		border: solid 1px grey;
+	}
+</style>
+
+	<h1 use:dragMe >
+		Drag Me
+	</h1>
+
