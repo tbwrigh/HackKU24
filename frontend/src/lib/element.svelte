fb8c38745e4339494053d8a7a633d5b131ce339c
b-sharman <byron.n.sharman@gmail.com>
2024-04-13 23:24:37 -0500

    Element component: images working

--- frontend/src/lib/element.svelte
+++ frontend/src/lib/element.svelte
@@ -1,17 +1,19 @@
 <script lang="ts">
   import type { Pair } from '$lib/types.ts';
+  import File from '$lib/file.svelte';
 
   export let pair: Pair;
   export let one_or_two: number;
+
 </script>
 
 <div>
   {#if one_or_two==1 && pair.object_one_type=="file"}
-    <img src={pair.object_one_value} />
+    <File patient_id={pair.patient_id} filename={pair.object_one_value} />
   {:else if one_or_two==1 && pair.object_one_type=="string"}
     <p>{pair.object_one_value}</p>
   {:else if one_or_two==2 && pair.object_two_type=="file"}
-    <img src={pair.object_two_value} />
+    <File patient_id={pair.patient_id} filename={pair.object_two_value} />
   {:else if one_or_two==2 && pair.object_two_type=="string"}
     <p>{pair.object_two_value}</p>
   {:else}
---
+++ frontend/src/lib/file.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+  export let patient_id;
+  export let filename;
+
+  let src;
+
+  onMount(async () => {
+    try {
+      const response = await fetch(`${backendUrl}/patient/${patient_id}/download/${filename}`);
+      src = URL.createObjectURL(await response.blob());
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  });
+</script>
+
+<img {src} alt={filename} />
