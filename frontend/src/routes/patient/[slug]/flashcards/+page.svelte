bb294d6aca86dffbb81a96a1c9d7c0ddd4d47699
b-sharman <byron.n.sharman@gmail.com>
2024-04-14 00:20:33 -0500

    flashcards: properly initialize the pair

--- frontend/src/routes/patient/[slug]/flashcards/+page.svelte
+++ frontend/src/routes/patient/[slug]/flashcards/+page.svelte
@@ -2,8 +2,16 @@
   import type { Pair } from '$lib/types';
   import Flashcard from '$lib/flashcard.svelte';
 
-  const pair = Pair(1, 1, "string", "a", "string", "b");
+  const pair: Pair = {
+    id: 1,
+    patient_id: 1,
+    object_one_type: "string",
+    object_one_value: "a",
+    object_two_type: "string",
+    object_two_value: "b",
+  };
 
 </script>
 
+  <p>Hello, can you see this?</p>
 <Flashcard {pair} />
