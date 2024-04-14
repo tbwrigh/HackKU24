abea61bf320f9fd058dab518e76017ad83b42e75
b-sharman <byron.n.sharman@gmail.com>
2024-04-13 22:00:05 -0500

    adjust to new API route path

--- frontend/src/routes/patient/[slug]/matching/+page.ts
+++ frontend/src/routes/patient/[slug]/matching/+page.ts
@@ -5,7 +5,7 @@ const backendUrl = import.meta.env.VITE_BACKEND_URL;
 export const load: PageLoad = async ({ fetch, params }) => {
   let json: null | JSON = null;
   try {
-    const response = await fetch(`${backendUrl}/pair/${params.slug}`);
+    const response = await fetch(`${backendUrl}/pair/patient/${params.slug}`);
     json = await response.json();
   } catch (error) {
     console.error('Error:', error);
