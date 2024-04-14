11cc1bd01e2efaa5a79c78805825d371ebaa3c2a
b-sharman <byron.n.sharman@gmail.com>
2024-04-14 04:28:48 -0500

    flashcards: performance optimization

--- frontend/src/lib/flashcard.svelte
+++ frontend/src/lib/flashcard.svelte
@@ -11,15 +11,12 @@
 
 <div class="flashcard-container p-4 min-w-[36rem] min-h-[20rem] max-w-[36rem] max-h-[20rem]">
   <Card on:click={() => flipped = !flipped} style={`perspective: 700px; text-align: center; transition: transform 0.3s; ${flipped ? "transform: rotateY(0.5turn)" : ""}`} class="w-full max-w-full h-full max-h-full justify-center">
-    {#if flipped}
-      <div style="transform: rotateY(0.5turn)">
-        <Element {pair} one_or_two={1} />
-      </div>
-    {:else}
-      <div>
-        <Element {pair} one_or_two={2} />
-      </div>
-    {/if}
+    <div style="transform: rotateY(0.5turn)" class:hidden={!flipped}>
+      <Element {pair} one_or_two={1} />
+    </div>
+    <div class:hidden={flipped}>
+      <Element {pair} one_or_two={2} />
+    </div>
   </Card>
 </div>
 
