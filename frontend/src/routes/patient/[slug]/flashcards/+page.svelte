577f3e57cbaf047a63d863d2dae82f26a7747e2b
daghassi <79544966+daghassi@users.noreply.github.com>
2024-04-14 00:32:49 -0500

    flashcard backend pt3 *works*

--- frontend/src/routes/patient/[slug]/flashcards/+page.svelte
+++ frontend/src/routes/patient/[slug]/flashcards/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   import type { Pair } from '$lib/types';
+  import type { PageData } from './$types';
   import Flashcard from '$lib/flashcard.svelte';
+  export let data: PageData;
+
 
   const pair: Pair = {
     id: 1,
@@ -13,5 +16,6 @@
 
 </script>
 
-  <p>Hello, can you see this?</p>
-<Flashcard {pair} />
+{#each data.pairs as pair, i}
+  <Flashcard {pair} />
+{/each}
--- frontend/src/routes/patient/[slug]/flashcards/+page.ts
+++ frontend/src/routes/patient/[slug]/flashcards/+page.ts
@@ -5,7 +5,7 @@ const backendUrl = import.meta.env.VITE_BACKEND_URL;
 export const load: PageLoad = async ({ fetch, params }) => {
   let json: null | JSON = null;
   try {
-    const response = await fetch(`${backendUrl}/content/get_pairs_by_patient_id_pair_patient_id_get`);
+    const response = await fetch(`${backendUrl}/pair/patient/${params.slug}`);
     json = await response.json();
   } catch (error) {
     console.error('Error:', error);
