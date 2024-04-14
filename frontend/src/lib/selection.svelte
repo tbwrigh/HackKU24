bb90c6041ff7148303ce0f12c5a117948daf2b61
b-sharman <byron.n.sharman@gmail.com>
2024-04-14 04:39:25 -0500

    rebrand to transparency theme

--- frontend/src/lib/selection.svelte
+++ frontend/src/lib/selection.svelte
@@ -8,9 +8,9 @@
 
 <div class="flex w-dvw h-dvh justify-center items-center place-content-center">
   <div class="w-[65ch] flex flex-col gap-4">
-    <Heading tag="h1" class="text-4xl text-center text-black">{heading}</Heading>
+    <Heading tag="h1" class="text-4xl text-center text-white">{heading}</Heading>
     {#each options as option}
-      <Button href={option.href} size="lg" color="light" class="mx-4 hover:ml-8 hover:mr-0 hover:bg-white hover:shadow hover:font-bold transition-all" pill>{option.label}</Button>
+      <Button href={option.href} size="lg" class="mx-4 hover:ml-8 hover:mr-0 font-bold border-white border-2 hover:border-3 hover:font-extrabold transition-all" pill>{option.label}</Button>
     {/each}
   </div>
 </div>
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -2,7 +2,7 @@
   import "../app.css";
 </script>
 
-<header class="z-50 fixed w-full flex justify-center place-content-center py-5 bg-white">
-  <a href="/" alt="return to home page" class="font-extrabold text-xl">Our App Name</a>
+<header class="fixed w-full flex justify-center place-content-center py-5">
+  <a href="/" alt="return to home page" class="z-50 font-extrabold text-xl text-white">Our App Name</a>
 </header>
 <slot></slot>
