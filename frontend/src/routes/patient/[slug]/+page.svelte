2bfdcd5c27c31d3bd0842d0b4ae872f397e169d5
Tyler Wright <tyler.wright5146@gmail.com>
2024-04-13 19:48:20 -0500

    settings link from patient home

--- frontend/src/routes/patient/[slug]/+page.svelte
+++ frontend/src/routes/patient/[slug]/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import type { PageData } from './$types';
   import type { SelectItem } from '$lib/types';
+  import { Button } from 'flowbite-svelte';
+  import { CogOutline } from 'flowbite-svelte-icons';
   import Selection from '$lib/selection.svelte';
 
   export let data: PageData;
@@ -18,3 +20,9 @@
 </script>
 
 <Selection heading={`${data.name}'s Profile`} {options} />
+
+<div class="fixed bottom-4 right-4 z-50">
+<a href={`/manage/${data.slug}`}>
+  <CogOutline class="w-12 h-112 text-gray-800" />
+</a>
+</div>
