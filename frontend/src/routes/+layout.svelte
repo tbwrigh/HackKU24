bfa2fc1cd9bd6e4544eae345cef6d103d701d417
daghassi <79544966+daghassi@users.noreply.github.com>
2024-04-14 06:48:54 -0500

    name change

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -3,6 +3,6 @@
 </script>
 
 <header class="fixed w-full flex justify-center place-content-center py-5">
-  <a href="/" alt="return to home page" class="z-50 font-extrabold text-xl text-white">Our App Name</a>
+  <a href="/" alt="return to home page" class="z-50 font-extrabold text-xl text-white">Kioku</a>
 </header>
 <slot></slot>
